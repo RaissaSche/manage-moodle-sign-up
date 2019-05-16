cba7e48eab373691841b1a95d2f09afda850b07d
Raissa Scheeren <raissa.fofs@gmail.com>
2019-05-16 17:07:27 -0300

    freeing up the first two columns for user and password

--- src/br/com/ntm/managemoodlesignup/ManageMoodleSignUp.java
+++ src/br/com/ntm/managemoodlesignup/ManageMoodleSignUp.java
@@ -6,25 +6,9 @@ import java.util.List;
 
 public class ManageMoodleSignUp {
 
-    //Use collections to do this. For example:
-
-    //List<List<Integer>> dynamic2D = new ArrayList<List<Integer>>();
-
-    //dynamic2D.add(new ArrayList<Integer>());
-    //dynamic2D.add(new ArrayList<Integer>());
-    //dynamic2D.add(new ArrayList<Integer>());
-
-    //dynamic2D.get(0).add(5);
-    //dynamic2D.get(0).add(6);
-    //dynamic2D.get(0).add(7);
-
-    //System.out.println(dynamic2D.get(0).get(0)); // 5
-    //System.out.println(dynamic2D.get(0).get(1)); // 6
-    // System.out.println(dynamic2D.get(0).get(2)); // 7
-
 //ler do arquivo
 //guardar em uma matriz
-    //trim every variable!! V
+    //trim every variable!!
     //throw things from strings to a (dynamic) matrix!! update: use collections!!
     // ------------------------------------------------------------------------
 //adicionar as colunas que que preciso, jogando com as variáveis
@@ -34,7 +18,10 @@ public class ManageMoodleSignUp {
     public static void main(String[] args) {
 
         List<List<String>> spreadsheetInfo = new ArrayList<List<String>>();
-        int rowNumber = 0;
+        //separating the first lines, so that they can be used for username and password
+        spreadsheetInfo.add(new ArrayList<>());
+        spreadsheetInfo.add(new ArrayList<>());
+        int rowNumber = 2;
 
         try (BufferedReader inputStream = new BufferedReader(new
                 FileReader("C:/Users/733120/Desktop/Scratch (respostas).txt"));
@@ -47,6 +34,7 @@ public class ManageMoodleSignUp {
                 String auxLine[] = line.split("\t");
 
                 for(String info : auxLine){
+                    //add reorganizing here maybe?
                     info = info.trim();
                     spreadsheetInfo.get(rowNumber).add(info);
                 }
@@ -58,13 +46,13 @@ public class ManageMoodleSignUp {
                 outputStream.newLine();
 
                 //tem que ter um jeito mais inteligente de checar isso, na boa
-               // if(spreadsheetInfo.contains("email") || spreadsheetInfo.contains("e-mail")
-                //        || spreadsheetInfo.contains("Email") || spreadsheetInfo.get(0).get(5).contains("E-mail")){
+                if(spreadsheetInfo.contains("email") || spreadsheetInfo.contains("e-mail")
+                        || spreadsheetInfo.contains("Email") || spreadsheetInfo.get(0).get(5).contains("E-mail")){
 
-                //}
-                //else{
-                //    throw new EmailNotFoundException();
-               // }
+                }
+                else{
+                    throw new EmailNotFoundException();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
