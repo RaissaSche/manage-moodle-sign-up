73de0c01cc9bf45658275282d1c1e49a77ae624e
Raissa Scheeren <raissa.fofs@gmail.com>
2019-05-16 15:45:31 -0300

    program stops parsing if lines are only tabulations

--- src/br/com/ntm/managemoodlesignup/ManageMoodleSignUp.java
+++ src/br/com/ntm/managemoodlesignup/ManageMoodleSignUp.java
@@ -37,14 +37,14 @@ public class ManageMoodleSignUp {
         int rowNumber = 0;
 
         try (BufferedReader inputStream = new BufferedReader(new
-                FileReader("C:/Users/733120/Desktop/Scratch (respostas) - Respostas ao formulário 1.csv"));
+                FileReader("C:/Users/733120/Desktop/Scratch (respostas).txt"));
              BufferedWriter outputStream = new BufferedWriter(new
                      FileWriter("C:/Users/733120/Desktop/scratch.csv"))) {
             String line;
-            while ((line = inputStream.readLine()) != null) {
+            while ((line = inputStream.readLine()) != null || line.matches("\t")) {
 
                 spreadsheetInfo.add(new ArrayList<>());
-                String auxLine[] = line.split(",");
+                String auxLine[] = line.split("\t");
 
                 for(String info : auxLine){
                     info = info.trim();
@@ -58,13 +58,13 @@ public class ManageMoodleSignUp {
                 outputStream.newLine();
 
                 //tem que ter um jeito mais inteligente de checar isso, na boa
-                if(spreadsheetInfo.contains("email") || spreadsheetInfo.contains("e-mail")
-                        || spreadsheetInfo.contains("Email") || spreadsheetInfo.get(0).get(5).contains("E-mail")){
+               // if(spreadsheetInfo.contains("email") || spreadsheetInfo.contains("e-mail")
+                //        || spreadsheetInfo.contains("Email") || spreadsheetInfo.get(0).get(5).contains("E-mail")){
 
-                }
-                else{
-                    throw new EmailNotFoundException();
-                }
+                //}
+                //else{
+                //    throw new EmailNotFoundException();
+               // }
             }
         } catch (IOException e) {
             e.printStackTrace();
