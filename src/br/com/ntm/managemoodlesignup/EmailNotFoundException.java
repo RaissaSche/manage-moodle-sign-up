3ba283a241b32ee1fa014ac3137304154ac97228
Raissa Scheeren <raissa.fofs@gmail.com>
2019-05-16 15:43:16 -0300

    reading + exporting the files and storing infos in variables

---
+++ src/br/com/ntm/managemoodlesignup/EmailNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.ntm.managemoodlesignup;
+
+public class EmailNotFoundException extends RuntimeException{
+
+    public EmailNotFoundException() {
+        super("Coluna email não encontrada na tabela");
+    }
+}
---
+++ src/br/com/ntm/managemoodlesignup/ManageMoodleSignUp.java
@@ -0,0 +1,73 @@
+package br.com.ntm.managemoodlesignup;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManageMoodleSignUp {
+
+    //Use collections to do this. For example:
+
+    //List<List<Integer>> dynamic2D = new ArrayList<List<Integer>>();
+
+    //dynamic2D.add(new ArrayList<Integer>());
+    //dynamic2D.add(new ArrayList<Integer>());
+    //dynamic2D.add(new ArrayList<Integer>());
+
+    //dynamic2D.get(0).add(5);
+    //dynamic2D.get(0).add(6);
+    //dynamic2D.get(0).add(7);
+
+    //System.out.println(dynamic2D.get(0).get(0)); // 5
+    //System.out.println(dynamic2D.get(0).get(1)); // 6
+    // System.out.println(dynamic2D.get(0).get(2)); // 7
+
+//ler do arquivo
+//guardar em uma matriz
+    //trim every variable!! V
+    //throw things from strings to a (dynamic) matrix!! update: use collections!!
+    // ------------------------------------------------------------------------
+//adicionar as colunas que que preciso, jogando com as variáveis
+    //username password \firstname lastname email\ course1
+//exportar para csv (ler do original o nome e concatenar com algo tipo "moodle-ready")
+
+    public static void main(String[] args) {
+
+        List<List<String>> spreadsheetInfo = new ArrayList<List<String>>();
+        int rowNumber = 0;
+
+        try (BufferedReader inputStream = new BufferedReader(new
+                FileReader("C:/Users/733120/Desktop/Scratch (respostas) - Respostas ao formulário 1.csv"));
+             BufferedWriter outputStream = new BufferedWriter(new
+                     FileWriter("C:/Users/733120/Desktop/scratch.csv"))) {
+            String line;
+            while ((line = inputStream.readLine()) != null) {
+
+                spreadsheetInfo.add(new ArrayList<>());
+                String auxLine[] = line.split(",");
+
+                for(String info : auxLine){
+                    info = info.trim();
+                    spreadsheetInfo.get(rowNumber).add(info);
+                }
+                System.out.println(spreadsheetInfo.get(rowNumber));
+                System.out.println(spreadsheetInfo.get(rowNumber).get(0));
+                rowNumber++;
+
+                outputStream.write(line);
+                outputStream.newLine();
+
+                //tem que ter um jeito mais inteligente de checar isso, na boa
+                if(spreadsheetInfo.contains("email") || spreadsheetInfo.contains("e-mail")
+                        || spreadsheetInfo.contains("Email") || spreadsheetInfo.get(0).get(5).contains("E-mail")){
+
+                }
+                else{
+                    throw new EmailNotFoundException();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
