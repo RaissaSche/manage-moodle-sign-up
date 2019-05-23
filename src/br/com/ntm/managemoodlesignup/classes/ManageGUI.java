c86274161933ab60b91e15f636092c2a255a863e
Raissa Scheeren <raissa.fofs@gmail.com>
2019-05-23 16:43:10 -0300

    fixing the bug that happened when the file was not the correct extension

--- src/br/com/ntm/managemoodlesignup/classes/ManageGUI.java
+++ src/br/com/ntm/managemoodlesignup/classes/ManageGUI.java
@@ -5,33 +5,40 @@ import java.io.File;
 
 public class ManageGUI {
 
+    boolean showSelectPrompt = true;
+
     ManageFile manageFile = new ManageFile();
+    File file;
 
     public void manageGUI() {
 
         JFileChooser fileChooser = new JFileChooser();
-        int answer = fileChooser.showOpenDialog(null);
 
-        if (answer == JFileChooser.APPROVE_OPTION) {
-            File file = fileChooser.getSelectedFile();
+        while (showSelectPrompt) {
 
-            try {
-                manageFile.checkIfFileIsTSV(file.getAbsolutePath());
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(fileChooser,
-                        e.getMessage(),
-                        "Aviso!",
-                        JOptionPane.WARNING_MESSAGE);
+            int answer = fileChooser.showOpenDialog(null);
+
+            if (answer == JFileChooser.APPROVE_OPTION) {
+                file = fileChooser.getSelectedFile();
+
+                try {
+                    manageFile.checkIfFileIsTSV(file.getAbsolutePath());
+                    showSelectPrompt = false;
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(fileChooser,
+                            e.getMessage(),
+                            "Aviso!",
+                            JOptionPane.WARNING_MESSAGE);
+                }
             }
-
-            String courseInitials = JOptionPane.showInputDialog("Nome breve do curso:");
-            manageFile.readAndWriteFile(file.getAbsolutePath(), courseInitials);
-
-            JOptionPane.showMessageDialog(fileChooser,
-                    "Feito! O novo arquivo está na mesma pasta que o que você inseriu!",
-                    "Pronto!",
-                    JOptionPane.INFORMATION_MESSAGE);
         }
+        String courseInitials = JOptionPane.showInputDialog("Nome breve do curso:");
+        manageFile.readAndWriteFile(file.getAbsolutePath(), courseInitials);
 
+        JOptionPane.showMessageDialog(fileChooser,
+                "Feito! O novo arquivo está na mesma pasta que o que você inseriu!",
+                "Pronto!",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }
+
